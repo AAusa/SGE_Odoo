16ca5c5b80a4f5d1458b632acecd2f2cd9b82f60
AAusa <aa21.aausaberri87@iespabloserrano.com>
2023-01-10 10:43:22 +0000

    Modulo biblioteca

---
+++ biblioteca/models/models.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class lector(models.Model):
+    _name = 'biblioteca.lector'
+    _description = 'Representa el lector de un libro'
+
+    nombre = fields.Char('Nombre', required=True)
+    prestamos = fields.Integer(string='Prestamos', max=10)
+
+class prestamo(models.Model):
+    _name = 'biblioteca.prestamo'
+    _description = 'Representa el prestamo de un libro'
+    
+    fechaInicio = fields.Date(string='Fecha de inicio')
+    fechaFin = fields.Date(string='Fecha de fin')
+
+ class libro(models.Model):
+    _name = 'biblioteca.libro'
+    _description = 'Representa un libro'
+    
+    titulo = fields.Char('Titulo', required=True)
+    numeroDePaginas = fields.Integer(string='numero de paginas')  
+
+ class editorial(models.Model):
+    _name = 'biblioteca.editorial'
+    _description = 'Representa la editorial de un libro'
+    
+    nombre = fields.Char('Nombre', required=True)
+
+ class autor(models.Model):
+    _name = 'biblioteca.autor'
+    _description = 'Representa un autor'
+    
+    nombre = fields.Char('Nombre', required=True)
+    nacimiento = fields.Date(string='Fecha de nacimiento del autor')
+    edad = fields.Integer(string='Edad', compute='calculaEdad')
+
+    @api.depends
+    def calculaEdad(self):
+        for autor in self:
+            #calculamos edad
+
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
