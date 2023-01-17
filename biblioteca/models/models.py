bf55a250892148cea516c9de2eb9ab90f204b3db
AAusa <aa21.aausaberri87@iespabloserrano.com>
2023-01-17 11:30:24 +0000

    en views de biblioteca

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -9,8 +9,7 @@ class lector(models.Model):
 
     dni = fields.Char('Dni', required=True, size=7)
     nombre = fields.Char('Nombre', required=True)
-    prestamos = fields.Integer(string='Prestamos', max=10)
-    prestamo_ids = fields.One2many('garaje.prestamo', 'dni', string='lector-prestamo')
+    prestamos_ids = fields.One2many('biblioteca.prestamo', 'lector_id', string='lector-prestamo')
 
 
 class prestamo(models.Model):
@@ -18,45 +17,45 @@ class prestamo(models.Model):
     _description = 'Representa el prestamo de un libro'
 
     identification = fields.Integer(string='Id', max=10) 
-    id = fields.id(string='Identificador') 
     fechaInicio = fields.Date(string='Fecha de inicio')
     fechaFin = fields.Date(string='Fecha de fin')
-    lector_id = fields.Many2one('garaje.lector', 'id', string='prestamo-lector')
+    lector_id = fields.Many2one('biblioteca.lector', string='prestamo-lector')
 
 
- class libro(models.Model):
+class libro(models.Model):
     _name = 'biblioteca.libro'
     _description = 'Representa un libro'
 
-    isbn = fields.Char('Isbn', required=True, size=17)    
+    isbn = fields.Char('Isbn', required=True, size=17)
     titulo = fields.Char('Titulo', required=True)
     numeroDePaginas = fields.Integer(string='numero de paginas') 
-    editorial_ids = fields.One2many('garaje.editorial', 'isbn', string='libro-editorial')
-    autor_ids = fields.many2many('garaje.autor', 'isbn', string='libro-autor')
+    editorial_ids = fields.One2many('biblioteca.editorial', 'libro_id', string='libro-editorial')
+    autor_ids = fields.Many2many('biblioteca.autor', string='libro-autor')
    
 
- class editorial(models.Model):
+class editorial(models.Model):
     _name = 'biblioteca.editorial'
     _description = 'Representa la editorial de un libro'
     
     nombre = fields.Char('Nombre', required=True)
-    editorial_ids = fields.Many2one('garaje.libro', 'nombre', string='editorial-libro')
+    libro_id = fields.Many2one('biblioteca.libro', string='editorial-libro')
 
 
- class autor(models.Model):
+class autor(models.Model):
     _name = 'biblioteca.autor'
     _description = 'Representa un autor'
 
-    dni = fields.Char('Dni', required=True, size=7)
+    dni = fields.Char('Dni_autor', required=True, size=7)
     nombre = fields.Char('Nombre', required=True)
     nacimiento = fields.Date(string='Fecha de nacimiento del autor')
     edad = fields.Integer(string='Edad', compute='calculaEdad')
-    libro_ids = fields.many2many('garaje.libro', 'dni', string='autor-libro')
+    libro_ids = fields.Many2many('biblioteca.libro', string='autor-libro')
 
     @api.depends
     def calculaEdad(self):
         for autor in self:
             #calculamos edad
+            pass
 
 
 #     name = fields.Char()
