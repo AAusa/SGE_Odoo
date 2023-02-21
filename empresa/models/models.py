9715bfbdb6c5eb1f38b553ed942a4c3603cfa560
AAusa <aa21.aausaberri87@iespabloserrano.com>
2023-02-21 11:33:25 +0000

    solucionEdad

--- empresa/models/models.py
+++ empresa/models/models.py
@@ -34,10 +34,15 @@ class cliente(models.Model):
     edad = fields.Integer('Edad', required = True, compute='_get_annios') 
     direccion = fields.Text('Direccion', required = True)
     telefono = fields.Integer('Telefono', required = True, size=9)#api.constraints
-    @api.constrains('telefono')
+    
+
+    @api.constrains('dni','telefono')
     def _check_value(self):
-        if self.telefono < 600000000 or self.telefono > 700000000:
-            raise ValidationError('Debe empezar por 6 o 7 y tener longitud 9')
+        letras = "TRWAGMYFPDXBNJZSQVHLCKE"
+        if (self.telefono < 600000000 or self.telefono > 700000000):
+            raise ValidationError('tfno incorrecto')
+        if (len(self.dni) != 9 or letras[int(self.dni[:8]) % 23] != self.dni[8]):
+            raise ValidationError('dni incorrecto')
 
     @api.depends('fechaNacimiento') 
     def _get_annios(self): 
@@ -59,7 +64,7 @@ class factura(models.Model):
     @api.constrains('total')
     def _check_value(self):
         if len(str(self.total))>7:
-            raise ValidationError('Debe tener longitud menor o igual a 7')
+            raise ValidationError('Debe tener 4 números naturales y 2 decimales o menos')
     #Campos relacionales
     cliente_id = fields.Many2one('empresa.cliente', string='factura-cliente')
     producto_ids = fields.One2many('empresa.producto', 'factura_id', string='factura-producto')
@@ -72,8 +77,13 @@ class producto(models.Model):
     identificador = fields.Integer('Identificador', required = True)
     nombre = fields.Char(string='Nombre', required = True)
     descripcion = fields.Text('Descripcion', required = True)
-    precio = fields.Float('Precio', help='Coste total de producto') # 5 números naturales más 2 decimales
+    precio = fields.Float('Precio', help='Coste total de producto') # 4 números naturales más 2 decimales
     stock = fields.Integer('Stock', required = True)
+
+    @api.constrains('precio')
+    def _check_value(self):
+        if len(str(self.precio))>7:
+            raise ValidationError('Debe tener 4 números naturales y 2 decimales o menos')
 
     #Campos relacionales
     factura_id = fields.Many2one('empresa.factura', string='producto-factura')
@@ -85,7 +95,7 @@ class categoria(models.Model):
     _name = 'empresa.categoria'
     _description = 'Caracteristicas de una categoria'
 
-    nombre = fields. Selection (string = 'Nombre', selection =[('o','Ordenadores'),('s','Smartphones'),('a','Audiovisual'), ('p','Periféricos'),('t','Televisores'), ('g','Gaming')], default='l')
+    nombre = fields. Selection (string = 'Nombre', selection =[('o','Ordenadores'),('s','Smartphones'),('a','Audiovisual'), ('p','Periféricos'),('t','Televisores'), ('g','Gaming'), ('so','Software'), ('aam','Aplicaciones a medida'), ('r','Reparaciones'), ('pda','PDAs')], default='l')
     
     producto_ids = fields.One2many('empresa.producto', 'categoria_id', string='categoria-producto')
 
@@ -93,9 +103,15 @@ class proveedor(models.Model):
     _name = 'empresa.proveedor'
     _description = 'Características de un proveedor'
 
-    nif = fields.Char('Dni', required = True, size=9)
+    nif = fields.Char('Nif', required = True, size=9)
     nombre = fields.Char(string='Nombre', required = True)
     direccion = fields.Text('Direccion', required = True)
     telefono = fields.Integer('Telefono', required = True, size=9)
 
     producto_ids = fields.One2many('empresa.producto', 'proveedor_id', string='proveedor-producto')
+
+
+    @api.constrains('telefono')
+    def _check_value(self):
+        if self.telefono < 600000000 or self.telefono > 700000000:
+            raise ValidationError('Debe empezar por 6 o 7 y tener longitud 9')
